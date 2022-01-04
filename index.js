6cfc6da0a7b67ff8f2577cb65afb6384204a7abe
Trần Thành Long <longtt2705@gmail.com>
2022-01-04 19:33:46 +0700

    Test eslint success (#3)
    * Test failed yarn lint
    
    * Test eslint success

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
-const testEslint = 1
 app.get("/", (req, res) => {
 	res.send("Hello Worlddddd!")
 })
