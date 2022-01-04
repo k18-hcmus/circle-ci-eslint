739c81710a730fee4a75986b48f72ae585f00774
zigvyLong <longtt2705@gmail.com>
2022-01-04 19:30:33 +0700

    Test failed yarn lint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
-const testEslint = 0
+const testEslint = 1
 app.get("/", (req, res) => {
 	res.send("Hello Worlddddd!")
 })
